f08af325370090fa7c30b6168a59ad4c42032055
Bartosz Koniński <bkoninski@apptension.com>
2020-05-30 00:39:21 +0200

    AWS Workshops #2.

---
+++ aws-workshops-2/service/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+PROJECT_NAME=bk-hello-world
+AWS_DEFAULT_REGION=eu-west-1
+VERSION=1
+
+AWS_ACCOUNT_ID=$(aws-vault exec aws-workshops -- aws sts get-caller-identity --output text --query 'Account')
+REPO_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$PROJECT_NAME
+
+TASK_IMAGE_URI=$REPO_URI:$VERSION
+
+docker build -t "$TASK_IMAGE_URI" .
+
+# shellcheck disable=SC2091
+$(aws-vault exec aws-workshops -- aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+
+docker push "$TASK_IMAGE_URI"
